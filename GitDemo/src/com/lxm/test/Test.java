52b7aabaee6405b5b068177bebc1f872753ff4f2
lxm <lxm13145201@126.com>
2020-12-10 10:24:17 +0800

    11

--- GitDemo/src/com/lxm/test/Test.java
+++ GitDemo/src/com/lxm/test/Test.java
@@ -3,13 +3,12 @@ package com.lxm.test;
 /**
  * @author lxm
  * @date 2020/12/10 9:00
- * @description:测试git
+ * description:测试git
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println("Git 你好！");
         System.out.println("11111111");
         System.out.println("2222222");
-        System.out.println("999999999");
     }
 }
