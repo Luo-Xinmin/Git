515eeb7b8cb3eccf0f87eb0b81fd7c77ced08b2a
Luo-Xinmin <75711845+Luo-Xinmin@users.noreply.github.com>
2020-12-10 09:09:04 +0800

    Update Test.java

--- GitDemo/src/com/lxm/test/Test.java
+++ GitDemo/src/com/lxm/test/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Git 你好！");
         System.out.println("11111111");
+        System.out.println("2222222");
     }
 }
