b44455bf1544fc44f3e4690005bf1cd39f803e8f
lxm <lxm13145201@126.com>
2020-12-10 09:06:29 +0800

    添加测试demo

---
+++ GitDemo/src/com/lxm/test/Test.java
@@ -0,0 +1,12 @@
+package com.lxm.test;
+
+/**
+ * @author lxm
+ * @date 2020/12/10 9:00
+ * @description:测试git
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git 你好！");
+    }
+}
