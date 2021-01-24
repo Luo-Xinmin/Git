652a1c670f7af6e1b3bbccdf644949ee54485aab
Luo-Xinmin <75711845+Luo-Xinmin@users.noreply.github.com>
2021-01-24 19:02:12 +0800

    Update Test.java

--- GitDemo/src/com/lxm/test/Test.java
+++ GitDemo/src/com/lxm/test/Test.java
@@ -16,5 +16,6 @@ public class Test {
         System.out.println("添加一行!");
         System.out.println("hahahah");
         System.out.println("ttttt");
+        System.out.println("Luo-Xinmin");
     }
 }
